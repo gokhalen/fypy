0c44aa3cea8ad8bae29cfb5b255a3d1b9476c2bd
Nachiket Gokhale <gokhalen@gmail.com>
2020-07-21 18:27:28 +0530

    Changes to be committed: 	modified:   TODO 	deleted:    src/libshape/#jacobians.py# 	modified:   src/libshape/jacobians.py 	new file:   test 	deleted:    testcommand

--- src/libshape/jacobians.py
+++ src/libshape/jacobians.py
@@ -5,9 +5,13 @@ from ..libutils import Point
 
 
 
-def jacobian1d(x:Tuple(Point,Point))->Tuple[np.ndarray,float]:
+def jacobian1d(xx:Tuple(Point,Point))->Tuple[np.ndarray,float]:
     '''
     returns the Jacobian determinant and Jacobian of a 1D linear element living in 3D space
     '''
-    
-    return (jacdet,jacdet)
+    p1=xx[0]
+    p2=xx[1]
+    # get the distance between the points 
+    hh=p1.distance(p2)
+    jacdet=0.5*hh
+    return (np.array(jacdet),jacdet)
