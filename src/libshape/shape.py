694af49eb071c8d97c1adb44b4364579d87207dd
Nachiket Gokhale <gokhalen@gmail.com>
2020-07-28 18:12:46 +0530

    Changes to be committed: 	modified:   src/libshape/shape.py 	modified:   src/libtest/test_libshape.py

--- src/libshape/shape.py
+++ src/libshape/shape.py
@@ -5,46 +5,78 @@ Created on Fri Jul 17 13:56:06 2020
 @author: Nachiket Gokhale
 
 1D,2D,3D shape functions and derivatives over the parent domain
-Since shape functions are evaluated over the same integration points in 
-parent domain, they are cached
+One can consider cacheing these shape function routines using @lru_cache
+from functools because they are evaluated at the integration points
+which do not change. But we can avoid this by calling these routines when the 
+only element class is initialized
 
 """
 
 import functools
 from typing import List,Tuple
 
-TF1 = Tuple[float]
-TF2 = Tuple[float,float]
-TF3 = Tuple[float,float,float]
+TF1  = Tuple[float]
+TF2  = Tuple[float,float]
+TF3  = Tuple[float,float,float]
+# Generic tuple type containing arbitrary tuples
+TGEN = Tuple[float,...]
+
+# TSH = Tuple of shape functions. e.g. (N1,N2,..)
+TSH  = TGEN
+# TDER = Tuple of Tuples e.g. ((N1_x,N1_y),(N2_x,N2_y)...)
+TDER = Tuple[TGEN,...]
+TOUT = Tuple[TSH,TDER]
+
+
 
 TF4 = Tuple[float,float,float,float]
 TF8 = Tuple[float,float,float,float,float,float,float,float]
 
-@functools.lru_cache
-def shape1d(xi:TF1) -> TF2:
+def shape1d(xi:TF1) -> TOUT:
     '''
-    1D linear shape function
+    1D linear shape function and parent domain derivatives
     '''
-    N1 = (1/2)*(1-xi)
-    N2 = (1/2)*(1+xi)
-    return (N1,N2)
+    N1 = (1.0/2.0)*(1-xi[0])
+    N2 = (1.0/2.0)*(1+xi[0])
+    
+    shape = (N1,N2)
+    der   = ((-0.5,),(+0.5,))
+    return (shape,der)
 
 
-@functools.lru_cache
-def shape1d_der(xi:TF1)-> TF2:
+def shape2d(xi:TF2)->TOUT:
     '''
-    1D linear shape function derivatives
+    these shape functions can be made faster by computing quantities
+    like (1-x[1]) once and storing them
     '''
-    return (-0.5,0.5)
+    N1 = (1.0/4.0)*(1-xi[0])*(1-xi[1])
+    N2 = (1.0/4.0)*(1+xi[0])*(1-xi[1])
+    N3 = (1.0/4.0)*(1+xi[0])*(1+xi[1])
+    N4 = (1.0/4.0)*(1-xi[0])*(1+xi[1])
+    
+    shape = (N1,N2,N3,N4)
+    
+    # dij = derivative of the ith shape function in the jth direction
+    d11  = -(1.0/4.0)*(1-xi[1]) 
+    d12  = -(1.0/4.0)*(1-xi[0])
+    d21  =  (1.0/4.0)*(1-xi[1])
+    d22  = -(1.0/4.0)*(1+xi[0])
+    d31  =  (1.0/4.0)*(1+xi[1])
+    d32  =  (1.0/4.0)*(1+xi[0])
+    d41  = -(1.0/4.0)*(1+xi[1])
+    d42  =  (1.0/4.0)*(1-xi[0])
+    
+    D1 = (d11,d12)
+    D2 = (d21,d22)
+    D3 = (d31,d32)
+    D4 = (d41,d42)
+    
+    der = (D1,D2,D3,D4)
+       
+    return (shape,der)
+    
+    
 
-@functools.lru_cache
-def shape2d(xi:TF2)->TF4:
-    print('hello')
-    pass
-
-@functools.lru_cache
-def shape2d_der(xi:TF2):
-    pass
 
 @functools.lru_cache
 def shape3d(xi:TF3):
--- src/libtest/test_libshape.py
+++ src/libtest/test_libshape.py
@@ -1,18 +1,67 @@
-import unittest
+import unittest,functools,math,itertools
+import numpy as np
+
+from typing import Callable,Iterable
 from ..libshape.shape import *
 
+closetol=1e-12
+npallclose=functools.partial(np.allclose,atol=closetol)
 
 class TestLibShape(unittest.TestCase):
+
+    # tests for shape1d and shape2d .
+    # input for both is a point represented by a tuple of floats
+    # output is ( shape, der )
+    # shape: (N1,N2...)
+    # der  : ((N1x,N1y..),(N2x,N2y,...))
+
+    def compare_shape(self,fshape:Callable,idata:Iterable,exout:Iterable):
+        '''
+        fshape: callable to be tested shape1d or shape2d
+        idata:  points at which shape functions and their derivatives have to be tested
+        exout:  expected output 
+        actout: actual output
+        '''
+        *actout,=map(fshape,idata)
+        for ipt,((ash,ad),(exsh,exd)) in enumerate(zip(actout,exout)):
+            # ash, ad   = actual (a) test shape functions (sh) and derivatives (d)
+            # exsh, exd = expected (ex) shape functions (sh) and derivatives (d)
+            # convert to np
+            ash,ad,exsh,exd  = map(np.asarray,(ash,ad,exsh,exd))
+            # boolean arrays with True when actual == expected 
+            shclose          = np.isclose(ash,exsh,atol=closetol)
+            dclose           = np.isclose(ad,exd,atol=closetol)
+            # should not have any False elements
+            shfalse = 
     
     def test_shape1d(self):
-        self.assertEqual((0,1),shape1d(1))
-        self.assertEqual((1,0),shape1d(-1))
-        self.assertEqual((0.5,0.5),shape1d(0))
+        # points for which the testing is to be performed
+        pts=[]
+        pts.append((-1,))
+        pts.append((-0.5,))
+        pts.append((0.0,))
+        pts.append((0.5,))
+        pts.append((1.0,))
+        #pts=((-1,),(-0.5,),(0,),(+0.5,),(1,))
 
-    def test_shape1d_der(self):
-        self.assertEqual((-0.5,0.5),shape1d_der(-1))
-        self.assertEqual((-0.5,0.5),shape1d_der(1))
-        self.assertEqual((-0.5,0.5),shape1d_der(1))
+        # expected values of shape functions at that point
+        nn=[]
+        nn.append((1.0,0.0)) 
+        nn.append((0.75,0.25))
+        nn.append((0.5,0.5))
+        nn.append((0.25,0.75))
+        nn.append((0.0,1.0))
+
+        # for 1d shape functions the derivatives are constant
+        der=((-0.5,),(0.5,))
+    
+        # expected output
+        *exout,=zip(nn,itertools.repeat(der))
+
+        self.compare_shape(shape1d,pts,exout)
+
+        
+
 
         
 
