f97885e9d4038d575be903e2ea5dfd815789106a
Nachiket Gokhale <gokhalen@gmail.com>
2020-09-03 15:57:19 +0530

    Changes to be committed: 	modified:   Problems/2D/10X10_Traction_Homegeneous/data.json.out 	modified:   Problems/2D/10X10_Traction_Homegeneous/ux.png 	new file:   fypy.py 	new file:   fypybject.py

---
+++ fypy.py
@@ -0,0 +1,24 @@
+from .src.libmesh.fypymesh import *
+from .src.libassem.assembly import *
+from .src.libsolve.fypysolver import *
+
+class FyPy():
+    def __init__(self,fin):
+        self.fypymesh = FyPyMesh()
+        self.fypymesh.json_read(fin)
+        self.outfile  = fin.strip('in') + 'out'
+
+    def assembly(self):
+        self.kk,self.rhs = assembly(self.fypymesh)
+
+    def solve(self,method='bicg'):
+        solver = FyPySolver(self.kk,self.rhs);
+        self.solution = solver.solve(method)
+
+    def output(self):
+        self.fypymesh.make_solution_from_rhs(self.solution)
+        self.fypymesh.make_output(self.outfile)
+
+    
+
+    
